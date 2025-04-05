6935d87f6f17384e096395d4897bccc57adfd55f
juanacev <137401190+juanacev@users.noreply.github.com>
2025-04-05 14:43:22 +0200

    Proyecto Inicial

---
+++ src/main/java/es/uma/taw/arkhammovies/ArkhamMoviesApplication.java
@@ -0,0 +1,13 @@
+package es.uma.taw.arkhammovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkhamMoviesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArkhamMoviesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/uma/taw/arkhammovies/ArkhamMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uma.taw.arkhammovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkhamMoviesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
